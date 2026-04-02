b6e300de48b2d463c45fecbb74928de7f4c0a812
jamal bajady <you@example.com>
2026-04-02 18:12:22 +0100

    pandas final

--- pandas/ex01/ex01.py
+++ pandas/ex01/ex01.py
@@ -1,18 +1,14 @@
 import numpy as np 
 import pandas as pd
+ 
 
 
-colors=np.array(["ss","ss2"])
-print(colors)
-
-d = {"col1": [0, 1, 2, 3], "col2": pd.Series([2, 3], index=[2, 3])}
-res= pd.DataFrame(data=d, index=[0, 1, 2, 3])
-ist = pd.Series([1, 2, 3, 4, 5])
-data = {"b": 1, "a": 0, "c": 2}
-s_dict = pd.Series(data)
-print(s_dict )
-#    col1  col2
-# 0     0   NaN
-# 1     1   NaN
-# 2     2   2.0
-# 3     3   3.0
+data=np.array([
+                ["Blue",    [1, 2],    1.1]
+               ,["Red",    [3, 4],    2.2]
+               ,["Pink",    [5, 6],    3.3]
+               ,["Grey",    [7, 8],    4.4]
+               ,["Black",[9, 10],5.5]],dtype=object)
+result=pd.DataFrame(data, columns=["color","list","number"], index=[1,3,5,7,9])
+print(result)
+print(result.dtypes)
