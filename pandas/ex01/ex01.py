816d3148e744439bee2493458dee4f5a10ac1faf
jamal bajady <you@example.com>
2026-04-03 17:16:22 +0100

    final visation

--- pandas/ex01/ex01.py
+++ pandas/ex01/ex01.py
@@ -11,4 +11,18 @@ data=np.array([
                ,["Black",[9, 10],5.5]],dtype=object)
 result=pd.DataFrame(data, columns=["color","list","number"], index=[1,3,5,7,9])
 print(result)
+
+df = pd.DataFrame({
+    'color'  : pd.Series(['Blue', 'Red', 'Pink', 'Grey', 'Black']),
+    'list'   : pd.Series([[1,2], [3,4], [5,6], [7,8], [9,10]]),
+    'number' : pd.Series([1.1, 2.2, 3.3, 4.4, 5.5]),
+    'number1': pd.Series([1, 3, 5, 7, 9])
+})
+print(df)
 print(result.dtypes)
+
+
+
+
+
+
